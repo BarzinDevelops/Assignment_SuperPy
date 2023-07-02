745715ac7749ef227d89e90917c03e041898aac7
Barzin <barzin.development@gmail.com>
2023-07-02 18:36:03 +0200

    Add function for calculating revenue and profit and save result to 'inventory.csv', by reading both: 'bought.csv' and 'sold.csv' data.

--- superpy/backup_code.py
+++ superpy/backup_code.py
@@ -123,61 +123,322 @@ if __name__ == "__main__":
     
     
     
-    """ 
+""" 
     # Imports
-import os
-import argparse
-import csv
-from datetime import date
-# using pandas to read from and write to files:
-import pandas as pd
+    import os
+    import argparse
+    import csv
+    from datetime import date
+    # using pandas to read from and write to files:
+    import pandas as pd
 
-# Do not change these lines.
-__winc_id__ = "a2bc36ea784242e4989deb157d527ba0"
-__human_name__ = "superpy"
-
-
-# Your code below this line.
+    # Do not change these lines.
+    __winc_id__ = "a2bc36ea784242e4989deb157d527ba0"
+    __human_name__ = "superpy"
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        description="Supermarket Inventory Tool.",
-        )
+    # Your code below this line.
+
+
+    def main():
+        parser = argparse.ArgumentParser(
+            description="Supermarket Inventory Tool.",
+            )
+        
+        # indicating: to buy or to sell
+        parser.add_argument(
+            '--action', 
+            choices=['buy', 'sel'],
+            metavar='buy or sel',          
+            type=str, 
+            help='Choose if you want to buy or sel the product.'
+            )
+
+        # getting arguments
+        parser.add_argument(
+            '-pn', '--prod_name', 
+            metavar='', 
+            type=str, 
+            help='Name of the product to add.'
+            )
+        parser.add_argument(
+            '-pp', '--price',
+            metavar='', 
+            type=float, 
+            help='Price of the product to add.'
+            )
+        parser.add_argument(
+            '-exp', '--expires',
+            metavar='', 
+            type=str, 
+            default="None",
+            help='The date of expiration (e.g., 2023-06-18)'
+            )
+        
+        args = parser.parse_args()
+        
+        read_or_create_csv_file('buy.csv')
+#--------------------------------------------
+
+
+    # reading from the file if exists, otherwise create it:
+    def read_or_create_csv_file(filename):
+        try:
+            data = pd.read_csv(filename)
+            # data['id'] = range(1, len(data) + 1)
+            # data.set_index('id', inplace=True)
+            # data.to_csv('buy.csv', index=True)
+            if len(data) > 0:
+                print(f"The content of {filename} is: \n{data}")
+            else:
+                colnames = ",\n".join(data.columns)
+                print(f"This file has no data, only these columns:\n{colnames}")
+        except FileNotFoundError:
+            print(f"This file doesn't exist yet!")
+            create_custom_csv_file(filename)
+
+
+    def create_custom_csv_file(filename):
+        user_answer = input("\nDo you want to create a new csv file with your own columns? (enter: 'y' or 'n'): ").lower()
+        
+        if user_answer == 'y':
+            column_names = input("\nEnter column names you want (separated by commas): ").split(',')
+            try:
+                print(f"\nCreating your new csv file...")
+                df = pd.DataFrame(columns=column_names)
+                df.to_csv(filename, index=False)
+                print(f"\nThe file: {filename} is created.")
+            except FileExistsError:
+                print(f"\nThis file: {filename}, already exists!")
+        else:
+            print("\nNo problem. Good luck.")
     
-    # indicating: to buy or to sell
-    parser.add_argument(
-        '--action', 
-        choices=['buy', 'sel'],
-        metavar='buy or sel',          
-        type=str, 
-        help='Choose if you want to buy or sel the product.'
-        )
 
+    if __name__ == "__main__":
+        main()
+    
+"""
+    
+    
+    
+    
+""" 
     # getting arguments
-    parser.add_argument(
-        '-pn', '--prod_name', 
-        metavar='', 
-        type=str, 
-        help='Name of the product to add.'
+    # parser.add_argument(
+    #     '-pn', '--prod_name', 
+    #     metavar='', 
+    #     type=str, 
+    #     help='Name of the product to add.'
+    #     )
+    # parser.add_argument(
+    #     '-q', '--quantity', 
+    #     metavar='', 
+    #     type=str, 
+    #     help='How many of this product to add.'
+    #     )
+    # parser.add_argument(
+    #     '-pp', '--prod_price',
+    #     metavar='', 
+    #     type=float, 
+    #     help='Price of the product to add.'
+    #     )
+    # parser.add_argument(
+    #     '-exp', '--exp_date',
+    #     metavar='', 
+    #     type=str, 
+    #     default="None",
+    #     help='Date of expiration (e.g., 2023-06-18)'
+    #     )
+"""
+
+
+""" # ---------------Other version----------------------
+    
+    # Imports
+    import os
+    import argparse
+    import csv
+    from datetime import date
+    # using pandas to read from and write to files:
+    import pandas as pd
+
+    # Do not change these lines.
+    # __winc_id__ = "a2bc36ea784242e4989deb157d527ba0"
+    # __human_name__ = "superpy"
+
+
+    # Your code below this line.
+    #------------------------------------------
+
+    # Set pandas display options
+    pd.set_option('display.width', None)  # Allow unlimited width
+    pd.set_option('display.max_columns', None)  # Show all columns
+
+    # created 5 backspaces to pull a string back 5 places:
+    reverse_tab = '\b\b\b\b\b'
+
+
+
+    def main():
+        # To include the help messages for the subparser arguments in the overall help message when running python main.py -h, this class overrides the argparse default help formatter and provides a custom formatter class.
+        class CustomHelpFormatter(argparse.HelpFormatter):
+            def _format_action(self, action):
+                parts = super()._format_action(action)
+                if action.nargs != argparse.PARSER:
+                    # The modification ensures that the help messages for subparser arguments are properly indented.
+                    parts = parts.replace('\n', '\n    ')
+                return parts
+
+        # Formatter class that inherits from both argparse.RawTextHelpFormatter and CustomHelpFormatter. 
+        class CombinedHelpFormatter(argparse.RawTextHelpFormatter, CustomHelpFormatter):
+            pass
+
+        parser = argparse.ArgumentParser(
+            description="Supermarket Inventory Tool.",
+            formatter_class=CombinedHelpFormatter
+            )
+
+        subparsers = parser.add_subparsers(dest='action')
+    
+        buy_parser = subparsers.add_parser(
+            'buy', 
+            help='Buy action help message'
+            )
+        buy_parser.add_argument(
+            'product_name',
+            metavar='', 
+            type=str, 
+            help='Specify the name of the product.')
+        buy_parser.add_argument(
+            '-pa','--prod_amount', 
+            metavar='',
+            type=int, 
+            help='Specify the amount of the product you want.')
+        buy_parser.add_argument(
+            '-pp','-prod_price', 
+            metavar='',
+            type=float, 
+            help='Specify the price of the product.')
+        buy_parser.add_argument(
+            '-ed','-expire_date', 
+            metavar='',
+            type=str, 
+            help='Specify the expiration date of the product')
+
+ sell_parser = subparsers.add_parser('sell')
+        sell_parser.add_argument(
+            'sell_product_name', 
+            type=str, 
+            help='Specify the name of the product being sold')
+        sell_parser.add_argument(
+            'sell_product_quantity', 
+            type=int, 
+            help='Specify the quantity of the product being sold')
+        sell_parser.add_argument(
+            'sell_product_price', 
+            type=float, 
+            help='Specify the price of the product being sold') 
+
+        report_parser = subparsers.add_parser('report')
+        report_parser.add_argument(
+            'report_type', 
+            choices=['inventory', 'revenue', 'profit'], 
+            metavar='', 
+            type=str,
+            help=f'''Choose what kind of report you want: [buy or sel]'''
         )
-    parser.add_argument(
-        '-pp', '--price',
-        metavar='', 
+    
+        args = parser.parse_args()
+        
+        # setting values of a row:
+        current_date = date.today().strftime('%Y-%m-%d')
+        received_args_series = pd.Series([current_date,args.prod_name,args.quantity, args.prod_price, args.exp_date])
+
+        update_csv_data('buy.csv', received_args_series)
+        # read_or_create_csv_file('buy.csv')
+    #--------------------------------------------
+
+    # reading from the file if exists, otherwise create it:
+    def read_or_create_csv_file(filename):
+        try:
+            data = pd.read_csv(filename)
+            if len(data) > 0:
+                return data
+            else:
+                colnames = " | ".join(data.columns)
+                return f"This file: '{filename}' -> has no data yet, only these predefined columns:\n{colnames} |"
+            
+        except FileNotFoundError:
+            print(f"This file doesn't exist yet!")
+            create_custom_csv_file(filename)
+
+
+    def create_custom_csv_file(filename):
+        user_answer = input("\nDo you want to create a new csv file with your own columns? (enter: 'y' or 'n'): ").lower()
+        
+        if user_answer == 'y':
+            column_names = input("\nEnter column names you want (separated by commas): ").split(',')
+            try:
+                print(f"\nCreating your new csv file...")
+                df = pd.DataFrame(columns=column_names)
+                df.to_csv(filename, index=False)
+                print(f"\nThe file: {filename} is created.")
+            except FileExistsError:
+                print(f"\nThis file: {filename}, already exists!")
+        else:
+            print("\nNo problem. Good luck.")
+        
+    def update_csv_data(filename, new_data):
+        # Read existing CSV file into a DataFrame
+        data = pd.read_csv(filename)
+
+        # Generate an auto-incremented ID
+        new_id = len(data) + 1
+
+        # Create a new DataFrame with ID and other data
+        new_row = pd.DataFrame([[new_id] + list(new_data)], columns=['id'] + list(data.columns[1:]))
+
+        # Concatenate the new row with the existing DataFrame
+        updated_data = pd.concat([data, new_row], ignore_index=True)
+
+        # Write the updated DataFrame back to the CSV file
+        updated_data.to_csv(filename, index=False)
+    
+        # current_data = read_or_create_csv_file(filename)
+        # print(current_data.to_string(index=False, justify='center'))
+    if __name__ == "__main__":
+        main() 
+"""
+
+
+
+
+
+""" 
+    sell_parser = subparsers.add_parser('sell')
+    sell_parser.add_argument(
+        'sell_product_name', 
+        type=str, 
+        help='Specify the name of the product being sold')
+    sell_parser.add_argument(
+        'sell_product_quantity', 
+        type=int, 
+        help='Specify the quantity of the product being sold')
+    sell_parser.add_argument(
+        'sell_product_price', 
         type=float, 
-        help='Price of the product to add.'
-        )
-    parser.add_argument(
-        '-exp', '--expires',
-        metavar='', 
-        type=str, 
-        default="None",
-        help='The date of expiration (e.g., 2023-06-18)'
-        )
+        help='Specify the price of the product being sold') 
+"""
+
+
+""" 
     
-    args = parser.parse_args()
-    
-    read_or_create_csv_file('buy.csv')
+    # setting values of a row:
+    current_date = date.today().strftime('%Y-%m-%d')
+    received_args_series = pd.Series([current_date,args.prod_name,args.quantity, args.prod_price, args.exp_date])
+
+    update_csv_data('buy.csv', received_args_series)
+    # read_or_create_csv_file('buy.csv')
 #--------------------------------------------
 
 
@@ -185,14 +446,12 @@ def main():
 def read_or_create_csv_file(filename):
     try:
         data = pd.read_csv(filename)
-        # data['id'] = range(1, len(data) + 1)
-        # data.set_index('id', inplace=True)
-        # data.to_csv('buy.csv', index=True)
         if len(data) > 0:
-            print(f"The content of {filename} is: \n{data}")
+            return data
         else:
-            colnames = ",\n".join(data.columns)
-            print(f"This file has no data, only these columns:\n{colnames}")
+            colnames = " | ".join(data.columns)
+            return f"This file: '{filename}' -> has no data yet, only these predefined columns:\n{colnames} |"
+        
     except FileNotFoundError:
         print(f"This file doesn't exist yet!")
         create_custom_csv_file(filename)
@@ -213,8 +472,27 @@ def create_custom_csv_file(filename):
     else:
         print("\nNo problem. Good luck.")
     
+def update_csv_data(filename, new_data):
+    # Read existing CSV file into a DataFrame
+    data = pd.read_csv(filename)
 
-if __name__ == "__main__":
-    main()
+    # Generate an auto-incremented ID
+    new_id = len(data) + 1
+
+    # Create a new DataFrame with ID and other data
+    new_row = pd.DataFrame([[new_id] + list(new_data)], columns=['id'] + list(data.columns[1:]))
+
+    # Concatenate the new row with the existing DataFrame
+    updated_data = pd.concat([data, new_row], ignore_index=True)
+
+    # Write the updated DataFrame back to the CSV file
+    updated_data.to_csv(filename, index=False)
+
+
+
+
     
-    """
+    # current_data = read_or_create_csv_file(filename)
+    # print(current_data.to_string(index=False, justify='center'))
+
+"""
--- superpy/main.py
+++ superpy/main.py
@@ -14,64 +14,114 @@ import pandas as pd
 # Your code below this line.
 #------------------------------------------
 
+line = '-'*20 # for underlining some columns 
+
 # Set pandas display options
 pd.set_option('display.width', None)  # Allow unlimited width
 pd.set_option('display.max_columns', None)  # Show all columns
 
+# created 5 backspaces to pull a string back 5 places:
+reverse_tab = '\b\b\b\b\b'
+reverse_tab2 = '\b\b\b\b\b\b\b\b\b\b'
+reverse_tab3 = '\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b'
+
+
 def main():
+    
     parser = argparse.ArgumentParser(
         description="Supermarket Inventory Tool.",
-        )
-    
-    # indicating: to buy or to sell
-    parser.add_argument(
-        '--action', 
-        choices=['buy', 'sel'],
-        metavar='buy or sel',          
-        type=str, 
-        help='Choose if you want to buy or sel the product.'
+        formatter_class=argparse.RawTextHelpFormatter
         )
 
-    # getting arguments
-    parser.add_argument(
-        '-pn', '--prod_name', 
-        metavar='', 
-        type=str, 
-        help='Name of the product to add.'
+    subparsers = parser.add_subparsers(dest='action')
+ 
+    buy_parser = subparsers.add_parser(
+        'buy', 
+        help='Buy action help message'
         )
-    parser.add_argument(
-        '-q', '--quantity', 
-        metavar='', 
-        type=str, 
-        help='How many of this product to add.'
-        )
-    parser.add_argument(
-        '-pp', '--prod_price',
-        metavar='', 
+    buy_parser.add_argument(
+        'buy_name',
+        # metavar='',
+        type=str,
+        help='Specify the name of the product.'
+    )
+    buy_parser.add_argument(
+        'buy_amount', 
+        # metavar='',
+        type=int, 
+        help='Specify the amount of the product you want.')
+    buy_parser.add_argument(
+        'buy_price', 
+        # metavar='',
         type=float, 
-        help='Price of the product to add.'
-        )
-    parser.add_argument(
-        '-exp', '--exp_date',
-        metavar='', 
+        help='Specify the price of the product.')
+    buy_parser.add_argument(
+        '--expire_date', 
+        metavar='',
         type=str, 
-        default="None",
-        help='Date of expiration (e.g., 2023-06-18)'
-        )
-    
+        help='Provide expire date (year-month-day)-> 2013-05-23 ')
+
+    sell_parser = subparsers.add_parser('sell')
+    sell_parser.add_argument(
+        'sell_name', 
+        type=str, 
+        help='Specify the name of the product being sold')
+    sell_parser.add_argument(
+        'sell_amount', 
+        type=int, 
+        help='Specify the amount of the product being sold')
+    sell_parser.add_argument(
+        'sell_price', 
+        type=float, 
+        help='Specify the price of the product being sold')
+
+    report_parser = subparsers.add_parser('report')
+    report_parser.add_argument(
+        'report_type', 
+        choices=['inventory', 'revenue', 'profit'], 
+        metavar='report_type', 
+        type=str,
+        help=f"""Choose what kind of report you want: [inventory, revenue, profit]"""
+    )
+
     args = parser.parse_args()
     
     # setting values of a row:
     current_date = date.today().strftime('%Y-%m-%d')
-    received_args_series = pd.Series([current_date,args.prod_name,args.quantity, args.prod_price, args.exp_date])
+    # determine if the input is for buy/sell and set the received_args_series accordingly:
+    print(args)
+    if args.action=='buy':
+        print(f"""
+              {reverse_tab3}bought product name:    {args.buy_name}
+              {reverse_tab3}bought product amount:  {args.buy_amount}
+              {reverse_tab3}bought product price:   {args.buy_price}
+              {reverse_tab3}product expire date:    {args.expire_date}
+              """)
+        #if bought:
+        received_args_series = pd.Series([current_date,args.buy_name,args.buy_amount, args.buy_price, args.expire_date])
+        col_names = ['id', 'buy_date', 'buy_name', 'buy_amount', 'buy_price', 'expire_date']
+        update_csv_data('bought.csv', col_names, received_args_series)
+    elif args.action=='sell':
+        print(f"""
+              {reverse_tab3}sold product name:    {args.sell_name}
+              {reverse_tab3}sold product amount:  {args.sell_amount}
+              {reverse_tab3}sold product price:   {args.sell_price}
+              """)
+         #if sold:  
+        received_args_series = pd.Series([current_date,args.sell_name,args.sell_amount, args.sell_price])
+        col_names = ['id', 'sell_date', 'sell_name', 'sell_amount', 'sell_price']
+        update_csv_data('sold.csv', col_names, received_args_series, )
 
-    update_csv_data('buy.csv', received_args_series)
-    # read_or_create_csv_file('buy.csv')
+    # Example usage
+    calculate_revenue_profit('bought.csv', 'sold.csv', 'inventory.csv')
+    df =  pd.read_csv('inventory.csv')
+    print(f"\nFrom inventory.csv :\n{line}\n{df.to_string(index=False)}")
+
 #--------------------------------------------
 
 
 # reading from the file if exists, otherwise create it:
-def read_or_create_csv_file(filename):
+def read_or_create_csv_file(filename, col_names, new_data):
     try:
         data = pd.read_csv(filename)
         if len(data) > 0:
@@ -79,28 +129,35 @@ def read_or_create_csv_file(filename):
         else:
             colnames = " | ".join(data.columns)
             return f"This file: '{filename}' -> has no data yet, only these predefined columns:\n{colnames} |"
-        
     except FileNotFoundError:
         print(f"This file doesn't exist yet!")
-        create_custom_csv_file(filename)
+        print("You will be redirected to file creator...")
+        create_custom_csv_file(filename, col_names, new_data)
 
 
-def create_custom_csv_file(filename):
-    user_answer = input("\nDo you want to create a new csv file with your own columns? (enter: 'y' or 'n'): ").lower()
+def create_custom_csv_file(filename, col_names, new_data):
+    try:
+        print(f"\nCreating your new csv file...")
+        # new_data = pd.concat([pd.Series(['id']), new_data], ignore_index=True)
+        
+        # print(f"NEW DATA is NOW -----------------> \n{new_data}")
+        df = pd.DataFrame(columns=col_names)
+        df.to_csv(filename, index=False)
+        print(f"\nThe file: {filename} is created.")
+        # read_or_create_csv_file(filename)
+    except FileExistsError:
+        print(f"\nThis file: {filename}, already exists!")
+    except ValueError as e:
+            print(e)
     
-    if user_answer == 'y':
-        column_names = input("\nEnter column names you want (separated by commas): ").split(',')
-        try:
-            print(f"\nCreating your new csv file...")
-            df = pd.DataFrame(columns=column_names)
-            df.to_csv(filename, index=False)
-            print(f"\nThe file: {filename} is created.")
-        except FileExistsError:
-            print(f"\nThis file: {filename}, already exists!")
-    else:
-        print("\nNo problem. Good luck.")
+def update_csv_data(filename, col_names, new_data):
+    try:
+        read_or_create_csv_file(filename, col_names, new_data)
+    except Exception as e:
+        # Handle the error
+        print("An error in UPDATE_CSV_DATA() function ---->", e)
     
-def update_csv_data(filename, new_data):
+    
     # Read existing CSV file into a DataFrame
     data = pd.read_csv(filename)
 
@@ -116,16 +173,32 @@ def update_csv_data(filename, new_data):
     # Write the updated DataFrame back to the CSV file
     updated_data.to_csv(filename, index=False)
 
-
-
-
-    
     # current_data = read_or_create_csv_file(filename)
     # print(current_data.to_string(index=False, justify='center'))
 
+def calculate_revenue_profit(bought_filename, sold_filename, inventory_filename):
 
-        
-        
+    # Read bought and sold data from CSV files
+    bought_data = pd.read_csv(bought_filename)
+    sold_data = pd.read_csv(sold_filename)
+
+    print(f"\nfrom bought.csv:\n{line}\n{bought_data.to_string(index=False)}")
+    print(f"\nfrom sold.csv:\n{line}\n{sold_data.to_string(index=False)}")
+    
+    # Calculate revenue by summing the sold price multiplied by the sold amount
+    sold_data['revenue'] = sold_data['sell_price'] * sold_data['sell_amount']
+
+    # Merge the bought and sold data on the product name
+    merged_data = pd.merge(bought_data, sold_data, left_on='buy_name', right_on='sell_name', how='left')
+
+    # Calculate profit by subtracting the bought price multiplied by the sold amount from the revenue
+    merged_data['profit'] = merged_data['revenue'] - (merged_data['buy_price'] * merged_data['sell_amount'])
+
+    # Save the inventory data (including revenue and profit) to the inventory CSV file
+    inventory_data = merged_data[['buy_name', 'buy_amount', 'buy_price', 'revenue', 'profit']]
+    inventory_data.to_csv(inventory_filename, index=False)
+
+
 
 if __name__ == "__main__":
     main()
